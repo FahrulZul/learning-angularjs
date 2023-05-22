74090353d7eab4cc53843a3d53915b945a33b076
fahrulzul <m.fahrulzul@gmail.com>
2023-05-22 18:04:44 +0800

    Update pages

--- app/app.js
+++ app/app.js
@@ -1,24 +1,49 @@
-const myModule = angular.module("myModule", []);
+const myModule = angular.module("myModule", ["ngRoute"]);
+
+const carsData = [
+	{ name: "Nissan GTR", price: 23000, imgUrl: "./assets/nissan-gtr.png" },
+	{ name: "Toyota Supra", price: 34000, imgUrl: "./assets/toyota-supra.png" },
+	{ name: "Audi TT", price: 26000, imgUrl: "./assets/audi-tt.png" },
+	{
+		name: "Nissan Fairlady",
+		price: 26000,
+		imgUrl: "./assets/nissan-fairlady.png",
+	},
+];
 
 // fire before application run
-myModule.config(async function () {});
+myModule.config([
+	"$routeProvider",
+	function ($routeProvider) {
+		$routeProvider
+			.when("/home", {
+				templateUrl: "views/home.html",
+				controller: "CarsController",
+			})
+			.when("/users", {
+				templateUrl: "views/userList.html",
+				controller: "CarsController",
+			})
+			.otherwise({
+				redirectTo: "/home",
+			});
+	},
+]);
 
 // fire when application run
 myModule.run(function () {});
 
-myModule.controller("FruitsController", [
-    "$scope",
-    function ($scope) {
-        $scope.fruits = ["Apple", "Banana", "Durian", "Rambutan"];
-    },
-]);
+myModule.controller("CarsController", [
+	"$scope",
+	function ($scope) {
+		$scope.cars = carsData;
 
-myModule.controller("UserController", [
-    "$scope",
-    function ($scope, users) {
-        console.log(users);
-
-        const newArr = [{ name: "Ali" }, { name: "Alia" }];
-        $scope.users = newArr;
-    },
+		$scope.addCar = function () {
+			console.log("hello world");
+			// $scope.cars.push({
+			// 	name: $scope.newCar.name,
+			// 	price: $scope.newCar.price,
+			// });
+		};
+	},
 ]);
